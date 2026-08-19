188a2ec1dee856cbb8be80ed0eecc1ecc2544244
Полина <tatarinova.polya2017@yandex.ru>
2026-08-19 17:54:09 +0300

    11 и 12 задачи

--- task11.ts
+++ task11.ts
@@ -3,7 +3,9 @@ import { Expect, Equal } from './helpers';
 // Задача: Добавить префикс ко всем ключам объекта.
 // Описание: Принимает объект T и строку P. Возвращает новый объект, в котором ко всем ключам T спереди добавлен префикс P.
 
-type AddPrefix<T, P extends string> = any;
+type AddPrefix<T, P extends string> = {
+    [K in keyof T as `${P}${K & string}`]: T[K]
+}
 
 // Проверки (не менять):
 type Input = { name: string; age: number };
--- task12.ts
+++ task12.ts
@@ -3,7 +3,9 @@ import { Expect, Equal } from './helpers';
 // Задача: Глубокий readonly (DeepReadonly).
 // Описание: Рекурсивно обходит объект T на всю глубину и делает все его свойства (и свойства всех вложенных объектов) доступными только для чтения (readonly).
 
-type DeepReadonly<T> = any;
+type DeepReadonly<T> = {
+    readonly[K in keyof T] : T[K] extends object ? DeepReadonly<T[K]> : T[K]
+}
 
 // Проверки (не менять):
 type Input = { a: { b: { c: number } } };
